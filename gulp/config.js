5f38d65eb982a36cf8ec5037d553a939dfb15825
James Vecore <james@byhook.com>
2014-08-19 18:28:54 -0400

    linting tests too

--- gulp/config.js
+++ gulp/config.js
@@ -22,7 +22,7 @@ var config = {
     }
   },
   lint: {
-    src: ["./app/**/*.js"]
+    src: ["./app/**/*.js", "./tests/**/*.js"]
   },
   scripts: {
 
--- tests/test_exampleModule.js
+++ tests/test_exampleModule.js
@@ -1,17 +1,17 @@
 'use strict';
 
 var assert         = require('chai').assert;
-var example_module = require('../app/example_module');
+var exampleModule = require('../app/exampleModule');
 
 suite('example_module', function(){
     setup(function(){
 
     });
 
-    suite("Greeter", function () {
+    suite('Greeter', function () {
         test('getGreeting should equal "hello world"', function(){
-            var g = new example_module.Greeter();
-            assert.strictEqual("hello world", g.getGreeting());
+            var g = new exampleModule.Greeter();
+            assert.strictEqual('hello world', g.getGreeting());
         });
     });
 });
