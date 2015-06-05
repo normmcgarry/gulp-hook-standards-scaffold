bd9ff8eda5f2c0f448bf020bc1de49d8fb172c58
“Jeremiah <jeremiah.reichardt@byhook.com>
2015-06-05 12:37:09 -0700

    build task reverted, version split out into its own task

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,56 +1,5 @@
 'use strict';
 
 var gulp = require('gulp');
-var gutil = require('gulp-util');
-var args = require('yargs').argv;
-var config = require('../config.js');
-var inject = require('gulp-inject-string');
-var rename = require('gulp-rename');
-var del = require('del');
-var replace = require('gulp-replace');
 
-gulp.task('build', ['tests', 'clean', 'static', 'scripts', 'styles', 'images'], function(){
-
-  if (args.version) {
-
-    var date = new Date();
-    var version;
-
-    if( typeof args.version === 'boolean' ){
-      version = date.getTime();
-    } else {
-      version = args.version.toString().replace( /[^\w.-]+/g ,"");
-    }
-
-    //inject the date and version into a new CSS file
-    gulp.src(config.styles.dist + 'index.css')
-      .pipe(inject.prepend('/* Created: ' + date + '*/\n'))
-      .pipe(inject.prepend('/* Version: ' + version + '*/\n'))
-      .pipe(rename('index.' + version + '.css'))
-      .pipe(gulp.dest(config.styles.dist));
-
-    //delete the old CSS file
-    del(config.styles.dist + 'index.css', {force: true});
-
-    //inject the date and version into a new JS file
-    gulp.src(config.scripts.dist + config.scripts.output)
-      .pipe(inject.prepend('/* Created: ' + date + ' */\n'))
-      .pipe(inject.prepend('/* Version: ' + version + ' */\n'))
-      .pipe(rename('main.build.' + version + '.js'))
-      .pipe(gulp.dest(config.scripts.dist));
-
-    //delete the old JS file
-    del(config.scripts.dist + config.scripts.output, {force: true});
-
-    //inject the date and version into the index.html file
-    //update references to the new CSS and JS files
-    gulp.src(config.static.dist + '/index.html')
-      .pipe(inject.append('<!-- Version: ' + version + ' -->\n'))
-      .pipe(inject.append('<!-- Created: ' + date + ' -->'))
-      .pipe(replace('index.css', 'index.' + version + '.css'))
-      .pipe(replace('<html ', '<html data-version="'+ version +'" '))
-      .pipe(replace(config.scripts.output, 'main.build.' + version + '.js'))
-      .pipe(gulp.dest(config.static.dist));
-
-  }
-});
+gulp.task('build', ['tests', 'clean', 'static', 'scripts', 'styles', 'images']);
