ab9bb7f109f620c7a528bf05b028364824ffd585
Jay Moretti <jay@byhook.com>
2015-06-06 10:24:07 -0300

    - images task streamlined

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -5,35 +5,32 @@ var browser = require('../browser');
 
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var changed = require('gulp-changed');
+// var changed = require('gulp-changed'); -- deprecated by gulp.lastRun
 var imagemin = require('gulp-imagemin');
 
-gulp.task('do-images', gulp.parallel(function () {
+function images(){
 
-	return gulp.src(config.images.src)
-		.pipe(changed(config.images.dist))
+	return gulp.src(config.images.src, {since: gulp.lastRun(images)})
+    //		.pipe(changed(config.images.dist)) --> deprecated by gulp.lastRun
 		.pipe(imagemin())
 		.pipe(gulp.dest(config.images.dist))
     .pipe(browser.reload({stream: true}));
 
-}));
+}
 
 
 // watch tasks
-
-gulp.task('reload-images', gulp.parallel('do-images', function () {
-
+function log(){
   gutil.log(gutil.colors.yellow('Reloading images...'));
+}
 
-}));
 
+function watch(){
+  if (config.watch) {
+    gulp.watch(config.images.src, gulp.series(log, images));
+  }
+}
 
 // main task
 
-gulp.task('images', gulp.parallel('do-images', function() {
-
-  if (config.watch) {
-    gulp.watch(config.images.src, gulp.series('reload-images'));
-  }
-
-}));
+gulp.task('images', gulp.parallel(images, watch));
