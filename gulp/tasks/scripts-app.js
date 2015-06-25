1f1af6712654c01d4e6f9954b43cef172b84b06d
“Jeremiah <jeremiah.reichardt@byhook.com>
2015-06-25 11:04:48 -0700

    scripts task redone

--- gulp/tasks/scripts-app.js
+++ gulp/tasks/scripts-app.js
@@ -1,26 +1,53 @@
+/**
+ * Merges all ( non LIB ) javascript files into one js file using browserify.
+ * @tasks/scripts-app
+ */
+
 'use strict';
 
-var config = require('../config.js');
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
+var gutil = require('gulp-util');
+var gulpif = require('gulp-if');
+var streamify = require('gulp-streamify');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('scripts-app', function(){
+/**
+ * @param gulp - function
+ * @param options - object
+ * options.entry : Path to the entry js file.
+ * options.dist : Destination directory for file output
+ * @param flags - object
+ * flags.minify : boolean
+ * flags.sourcemap : boolean
+ * @returns {Function}
+ */
+module.exports = function( gulp, options, flags ) {
 
-  var bundler = browserify(config.scripts.entry, {
-    debug: true,
-    cache: {}
-  });
+  return function(){
 
-  var rebundle = function() {
-    return bundler.bundle()
-      .pipe(source(config.scripts.output))
-      .pipe(buffer())
-      .pipe(gulp.dest(config.scripts.dist));
-  };
+    var bundler = browserify(options.entry, {
+      debug: flags.sourcemap,
+      cache: {}
+    });
 
-  bundler.on('update', rebundle);
+    var rebundle = function() {
+      return bundler.bundle()
+        .pipe(source(options.output))
+        .pipe(gulpif(flags.sourcemap, buffer()))
+        .pipe(gulpif(flags.sourcemap, sourcemaps.init({loadMaps: true})))
+        .pipe(gulpif(flags.minify, streamify(uglify())))
+        .pipe(gulpif(flags.sourcemap, sourcemaps.write('./')))
+        .pipe(gulp.dest(options.dist));
+    };
 
-  return rebundle();
-});
+    bundler.on('update', rebundle);
+
+    return rebundle();
+
+  }
+
+}
