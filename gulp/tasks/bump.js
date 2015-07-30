6f54699533327a588145266d9ac88f78b5127c93
SteveHamilton <steve@byhook.com>
2015-07-30 15:05:00 -0400

    whooopsies missed a file. The actual bump task.

--- gulp/tasks/bump.js
+++ gulp/tasks/bump.js
@@ -1,26 +1,21 @@
 /**
- * Runs jscs tests.
+ * Bumps package version.
  * @tasks/bump
  */
 
 'use strict';
 
 var bump = require('gulp-bump');
-var args = require('yargs').argv;
 
 /**
  * @param gulp - function
  * @param options - object
- * options.src : Files to validate.
+ * options.src : Source package file
  * @returns {Function}
  */
 module.exports = function( gulp, options ) {
 
   return function() {
-
-    var option = {
-      type: args.to
-    };
 
     return gulp.src( options.src )
       .pipe(bump())
